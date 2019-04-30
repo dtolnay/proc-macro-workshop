4ba084e05480761de9906472f8f773fc03bf34e7
David Tolnay <dtolnay@gmail.com>
2019-04-29 20:41:59 -0700

    debug: Solution

--- debug/tests/progress.rs
+++ debug/tests/progress.rs
@@ -1,12 +1,12 @@
 #[test]
 fn tests() {
     let t = trybuild::TestCases::new();
-    //t.pass("tests/01-parse.rs");
-    //t.pass("tests/02-impl-debug.rs");
-    //t.pass("tests/03-custom-format.rs");
-    //t.pass("tests/04-type-parameter.rs");
-    //t.pass("tests/05-phantom-data.rs");
-    //t.pass("tests/06-bound-trouble.rs");
-    //t.pass("tests/07-associated-type.rs");
-    //t.pass("tests/08-escape-hatch.rs");
+    t.pass("tests/01-parse.rs");
+    t.pass("tests/02-impl-debug.rs");
+    t.pass("tests/03-custom-format.rs");
+    t.pass("tests/04-type-parameter.rs");
+    t.pass("tests/05-phantom-data.rs");
+    t.pass("tests/06-bound-trouble.rs");
+    t.pass("tests/07-associated-type.rs");
+    t.pass("tests/08-escape-hatch.rs");
 }
