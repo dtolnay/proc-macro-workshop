a2ab68b12027a44f81e857a433493be305a2c7c8
David Tolnay <dtolnay@gmail.com>
2019-05-11 06:09:19 -0700

    Requirements for the bits-attribute-wrong message

--- bitfield/tests/11-bits-attribute-wrong.rs
+++ bitfield/tests/11-bits-attribute-wrong.rs
@@ -1,5 +1,8 @@
 // This is just the compile_fail version of the previous test case, testing what
 // error happens if the user has written an incorrect #[bits = N] attribute.
+//
+// Ensure that the error message points to the incorrect attribute and contains
+// the correct number of bits in some form.
 
 use bitfield::*;
 
