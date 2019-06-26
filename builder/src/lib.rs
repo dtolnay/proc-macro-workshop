031e1ac7ff5e0b887ff7e9db3ccfebf60b7fe62a
Rajiv Ranganath <rajiv.ranganath@atihita.com>
2019-06-26 12:51:13 +0000

    `derive(Builder)`: Test 01 Pass
    Signed-off-by: Rajiv Ranganath <rajiv.ranganath@atihita.com>

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,19 @@
 extern crate proc_macro;
+extern crate quote;
+extern crate syn;
 
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast: DeriveInput = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    eprintln!("{:#?}", ast);
+
+    let expanded = quote! {};
+    let _ = TokenStream::from(expanded);
+
+    TokenStream::new()
 }
