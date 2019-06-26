cbbfe69f06ad37ba4ff1218af42ae5e4ea9ee85f
Rajiv Ranganath <rajiv.ranganath@atihita.com>
2019-06-26 12:51:14 +0000

    `derive(Builder)`: Test 03, 04, 05 Pass
    Signed-off-by: Rajiv Ranganath <rajiv.ranganath@atihita.com>

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,3 +1,5 @@
+#![recursion_limit = "128"]
+
 extern crate proc_macro;
 extern crate quote;
 extern crate syn;
@@ -13,22 +15,78 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let bname: String = format!("{}Builder", name);
     let bindent: syn::Ident = Ident::new(&bname, name.span());
 
+    // &syn::punctuated::Punctuated<syn::Field, syn::token::Comma>
+    // implements `IntoIter`
+    let fields = if let syn::Data::Struct(syn::DataStruct {
+        fields: syn::Fields::Named(syn::FieldsNamed { ref named, .. }),
+        ..
+    }) = ast.data
+    {
+        named
+    } else {
+        // in case the #[derive(Builder)] is put on an `enum`
+        unimplemented!();
+    };
+
+    let optionized = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+
+        quote! {
+            #name: std::option::Option<#ty>
+        }
+    });
+
+    let methods = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+
+        quote! {
+            pub fn #name(&mut self, #name: #ty) -> &mut Self {
+                self.#name = Some(#name);
+                self
+            }
+        }
+    });
+
+    let build_fields = fields.iter().map(|f| {
+        let name = &f.ident;
+
+        quote! {
+            #name: self.#name.clone().ok_or(concat!(stringify!(#name), " is not set"))?
+        }
+    });
+
+    let build_empty = fields.iter().map(|f| {
+        let name = &f.ident;
+
+        quote! {
+            #name: None
+        }
+    });
+
     // quote::__rt::TokenStream
     let expanded = quote! {
         struct #bindent {
-            executable: Option<String>,
-            args: Option<Vec<String>>,
-            env: Option<Vec<String>>,
-            current_dir: Option<String>,
+            #(#optionized,)*
+        }
+
+        impl #bindent {
+            #(#methods)*
+
+            pub fn build(&self) -> Result<#name, Box<dyn std::error::Error>> {
+                Ok(
+                    #name {
+                        #(#build_fields,)*
+                    }
+                )
+            }
         }
 
         impl #name {
             fn builder() -> #bindent {
                 #bindent {
-                    executable: None,
-                    args: None,
-                    env: None,
-                    current_dir: None,
+                    #(#build_empty,)*
                 }
             }
         }
