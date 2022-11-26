1a925efc3b6254863408892cb833828615b11c01
Jiseok CHOI <jiseok.dev@gmail.com>
2022-11-26 19:42:35 +0900

    Create `{ident}Builder` struct

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,19 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput, Ident};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    let name_ident = &input.ident;
+    let builder_ident = Ident::new(&format!("{name_ident}Builder"), name_ident.span());
+
+    let mut custom_input = input.clone();
+    custom_input.ident = builder_ident;
+    let ts = quote! {
+        #custom_input
+    };
+
+    ts.into()
 }
