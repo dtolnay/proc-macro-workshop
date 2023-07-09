6e4abf57d00f6e1d313da8a3d393cdfe979fa34e
yourarj <11359226+yourarj@users.noreply.github.com>
2023-07-09 08:26:45 +0530

    ✅ 02-create-builder.rs
    	📝 added quote create for transforming back into TokenStream
    	📝 created struct for builder state
    	📝 implemented empty builder function for Target type
    
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     Date:      Sun Jul 9 08:26:45 2023 +0530
    
     On branch master
    
     Changes to be committed:
    	modified:   Cargo.toml
    	modified:   src/lib.rs
    	modified:   tests/progress.rs

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,12 +1,27 @@
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, DeriveInput};
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput, Ident};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     // parsing input tokenstream to DeriveInput
     let parsed_input = parse_macro_input!(input as DeriveInput);
 
-    println!("{:#?}", parsed_input);
+    // uncomment following line to see parsed tree
+    // println!("{:#?}", parsed_input);
 
-    TokenStream::new()
+    let target_struct_ident = parsed_input.ident;
+    let builder_strcut_name = format!("{}Builder", target_struct_ident);
+    let builder_struct_ident = Ident::new(&builder_strcut_name, target_struct_ident.span());
+
+    quote!(
+        struct #builder_struct_ident {}
+
+        impl #target_struct_ident {
+            fn builder() -> #builder_struct_ident {
+                #builder_struct_ident{}
+            }
+        }
+    )
+    .into()
 }
--- builder/tests/progress.rs
+++ builder/tests/progress.rs
@@ -2,7 +2,7 @@
 fn tests() {
     let t = trybuild::TestCases::new();
     t.pass("tests/01-parse.rs");
-    //t.pass("tests/02-create-builder.rs");
+    t.pass("tests/02-create-builder.rs");
     //t.pass("tests/03-call-setters.rs");
     //t.pass("tests/04-call-build.rs");
     //t.pass("tests/05-method-chaining.rs");
