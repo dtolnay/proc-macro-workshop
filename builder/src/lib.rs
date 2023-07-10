5fd654f149018f38a9cd210c118a5feb64008663
yourarj <11359226+yourarj@users.noreply.github.com>
2023-07-10 23:16:47 +0530

    ✅ builder/tests/05-method-chaining.rs ✅ builder/tests/06-optional-field.rs
                current status:
                ✅ builder/tests/01-parse.rs
                ✅ builder/tests/02-create-builder.rs
                ✅ builder/tests/03-call-setters.rs
                ✅ builder/tests/04-call-build.rs
                ✅ builder/tests/05-method-chaining.rs
                ✅ builder/tests/06-optional-field.rs
                ⚠️ builder/tests/07-repeated-field.rs
                ⚠️ builder/tests/08-unrecognized-attribute.rs
                ⚠️ builder/tests/08-unrecognized-attribute.stderr
                ⚠️ builder/tests/09-redefined-prelude-types.rs
    
                changes:
                📝 exempting wrapping already optional fields
    	    📝 helper for unwrapping type in option
    	    📝 setter of option type allows contained fields
    
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     Date:      Mon Jul 10 23:16:47 2023 +0530
    
     On branch master
    
     Changes to be committed:
    	modified:   builder/src/lib.rs
    	modified:   builder/tests/progress.rs

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput, Ident};
+use syn::{parse_macro_input, DeriveInput, GenericArgument, Ident};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -14,6 +14,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let builder_struct_name = format!("{}Builder", target_struct_ident);
     let builder_struct_ident = Ident::new(&builder_struct_name, target_struct_ident.span());
 
+    // get all named fields
     let fields = if let syn::Data::Struct(syn::DataStruct {
         fields: syn::Fields::Named(syn::FieldsNamed { ref named, .. }),
         ..
@@ -24,30 +25,103 @@ pub fn derive(input: TokenStream) -> TokenStream {
         unimplemented!()
     };
 
+    // utility closure to check if field is option
+    let is_option = |field: &syn::Type| {
+        if let syn::Type::Path(ref p) = field {
+            return p
+                .path
+                .segments
+                .last()
+                .map(|e| e.ident.to_string() == "Option")
+                == Some(true);
+        }
+        false
+    };
+
+    // utility function to unwrap the type within Option
+    let unwrap_optioned_type = |opt: &syn::Type| {
+        assert!(is_option(&opt));
+        if let syn::Type::Path(typath) = opt {
+            if let syn::PathArguments::AngleBracketed(ref inner) = typath.path.segments[0].arguments
+            {
+                inner
+                    .args
+                    .first()
+                    .map(|ga| {
+                        if let GenericArgument::Type(syn::Type::Path(syn::TypePath {
+                            qself: _,
+                            path,
+                        })) = ga
+                        {
+                            Some(
+                                path.segments
+                                    .first()
+                                    .expect("Option should contain type")
+                                    .ident
+                                    .clone(),
+                            )
+                        } else {
+                            None
+                        }
+                    })
+                    .unwrap_or(None)
+            } else {
+                unimplemented!()
+            }
+        } else {
+            None
+        }
+    };
+
+    // map and wrap all mandatory fields in option
     let optioned_fields = fields.iter().map(|f| {
         let field_name = &f.ident;
         let field_orig_type = &f.ty;
-        quote!( #field_name: core::option::Option<#field_orig_type>)
+
+        if is_option(field_orig_type) {
+            quote!(#field_name: #field_orig_type)
+        } else {
+            quote!( #field_name: core::option::Option<#field_orig_type>)
+        }
     });
 
+    // get all field idents
     let field_idents = fields.iter().map(|f| &f.ident);
 
-    let fn_getters = fields.iter().map(|f| {
+    // create field setters
+    let fn_setters = fields.iter().map(|f| {
         let item_name = &f.ident;
         let item_type = &f.ty;
-        quote! {
-            pub fn #item_name(&mut self, #item_name: #item_type) -> &mut Self {
+
+        if is_option(item_type) {
+            // extract option
+            let unwraped_type = unwrap_optioned_type(item_type).unwrap();
+
+            quote! {
+                pub fn #item_name(&mut self, #item_name: #unwraped_type) -> &mut Self{
                 self.#item_name = core::option::Option::Some(#item_name);
                 self
+                }
+            }
+        } else {
+            quote! {
+                pub fn #item_name(&mut self, #item_name: #item_type) -> &mut Self {
+                    self.#item_name = core::option::Option::Some(#item_name);
+                    self
+                }
             }
         }
     });
 
     let build_functions = fields.iter().map(|f| {
         let f_name = &f.ident;
+        if is_option(&f.ty){
+            quote!( #f_name: self.#f_name.clone())
+        } else{
         quote! {
-            #f_name: self.#f_name.clone().ok_or(concat!("field `", stringify!( #f_name), "` has not been initialized!"))
+            #f_name: self.#f_name.clone().ok_or(concat!("field `", stringify!( #f_name), "` has not been initialized!"))?
         }
+    }
     });
 
     // println!("{:?}", optioned_fields);
@@ -68,10 +142,10 @@ pub fn derive(input: TokenStream) -> TokenStream {
         impl #builder_struct_ident {
             pub fn build(&self) -> Result<#target_struct_ident, Box<dyn std::error::Error>> {
                 Ok(#target_struct_ident {
-                    #(#build_functions?),*
+                    #(#build_functions),*
                 })
             }
-            #(#fn_getters)*
+            #(#fn_setters)*
         }
     )
     .into()
